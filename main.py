e88b876f07bc7864597573172f900f420b41eddc
Moonwacht <sao105752@gmail.com>
2026-05-27 03:23:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 app = FastAPI()
 
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
-GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent"
 class ImagePayload(BaseModel):
     image: str
 
